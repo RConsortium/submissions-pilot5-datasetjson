ad5cf6de7291da986fab40bcc83068104bb864ca
SHAESEN2 <haesendonckx.steven@gmail.com>
2025-02-26 13:11:02 +0000

    1. Updated ADTTE spec for Predecessor variables 2. Included adam folder for submission 3. Initial draft of adtte updates

--- pilot5-submission/pilot5-programs/adtte.r
+++ pilot5-submission/pilot5-programs/adtte.r
@@ -26,6 +26,7 @@ library(metacore)
 library(metatools)
 library(pilot5utils)
 library(xportr)
+library(datasetjson)
 
 # read source -------------------------------------------------------------
 
@@ -34,13 +35,9 @@ adae <- read_xpt(file.path(path$adam, "adae.xpt"))
 ds <- convert_blanks_to_na(read_xpt(file.path(path$sdtm, "ds.xpt")))
 
 
-## placeholder for origin=predecessor, use metatool::build_from_derived()
-
-metacore <- spec_to_metacore(file.path(path$adam, "adam-pilot-3.xlsx"), where_sep_sheet = FALSE)
-
 # Get the specifications for the dataset we are currently building
 
-adtte_spec <- metacore %>%
+adtte_spec <- spec_to_metacore(path = file.path(path$adam, "adam-pilot-5.xlsx"), where_sep_sheet = FALSE) %>%
   select_dataset("ADTTE")
 
 # First dermatological event (ADAE.AOCC01FL = 'Y' and ADAE.CQ01NAM != '')
@@ -93,8 +90,9 @@ censor <- censor_source(
   )
 )
 
+# Finalize dataset + add common variables ---------------------------------
 
-adtte_pre <- derive_param_tte(
+pre_adtte <- derive_param_tte(
   dataset_adsl = adsl,
   start_date = TRTSDT,
   event_conditions = list(event),
@@ -122,17 +120,49 @@ adtte_pre <- derive_param_tte(
     TRTP = TRT01P
   )
 
-adtte <- adtte_pre %>%
+# Export dataset to xpt ---------------------------------------------------
+
+var_spec <- data.frame(
+  dataset = "adtte",
+  label = "AE Time To 1st Derm. Event Analysis",
+  data_label = "ADTTE"
+)
+
+adtte <- pre_adtte %>%
   drop_unspec_vars(adtte_spec) %>% # only keep vars from define
   order_cols(adtte_spec) %>% # order columns based on define
   set_variable_labels(adtte_spec) %>% # apply variable labels based on define
-  # xportr_type(adtte_spec, "ADTTE") %>%
-  # xportr_length(adtte_spec, "ADTTE") %>%
-  # unresolved issue in xportr_length due to:
-  # https://github.com/tidyverse/haven/issues/699
-  # no difference found by diffdf after commenting out xportr_length()
   xportr_format(adtte_spec$var_spec %>%
-    mutate_at(c("format"), ~ replace_na(., "")), "ADTTE") %>%
-  xportr_write(file.path(path$adam, "adtte.xpt"),
-    label = "AE Time To 1st Derm. Event Analysis"
-  )
+                  mutate_at(c("format"), ~ replace_na(., "")), "ADTTE")
+xportr_write(adtte,
+             path = file.path(path$adam, "adtte.xpt"),
+             domain = "adtte",
+             max_size_gb = 4, # PMDA submission restriction
+             metadata = var_spec,
+             strict_checks = FALSE
+)
+
+# Export dataset to JSON ---------------------------------------------------
+
+sysinfo <- unname(Sys.info()) 
+
+adtte_spec$value_spec
+
+ds_json <- dataset_json(
+  adtte,
+  file_oid = path$adam,
+  last_modified = strftime(as.POSIXlt(Sys.time(), "UTC"), "%Y-%m-%dT%H:%M"),
+  originator = "Steven Haesendonckx",
+  sys = sysinfo[1],
+  sys_version = sysinfo[3],
+  study = "CDSICPILOT01",
+  metadata_version = "MDV.MSGv2.0.SDTMIG.3.3.SDTM.1.7",
+  metadata_ref = "some/define.xml",
+  item_oid = "IG.IRIS",
+  name = "ADTTE",
+  dataset_label = "AE Time To 1st Derm. Event Analysis",
+  columns = adtte_spec$value_spec
+)
+
+
+# End ---------------------------------------------------------------------
