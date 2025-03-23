a0c7efd28b0037fca7c4cfc3951350a013167ab2
cpiraux <celinepiraux@gmail.com>
2025-03-23 07:20:35 +0000

    Update path to original

--- pilot5-submission/pilot5-programs/path.r
+++ pilot5-submission/pilot5-programs/path.r
@@ -7,9 +7,7 @@
 ###########################################################################
 # !/usr/bin/env Rscript
 path <- list(
-  sdtm = "./pilot5-submission/pilot5-input/sdtmdata", # Modify path to the sdtm location
-  adam = "./pilot5-submission/pilot5-input/adamdata", # Modify path to the adam location
-  adamspecs = "./pilot5-submission", # Modify path to the adam metadata location
-  output_tlfs = "./pilot5-submission/pilot5-output/pilot5-tlfs", # Modify path to the output location
-  output_datasetjson = "./pilot5-submission/pilot5-output/pilot5-datasetjson" # Modify path to the output location
+  sdtm = "./submission/sdtm", # Modify path to the sdtm location
+  adam = "./submission/adam", # Modify path to the adam location
+  output = "./submission/output" # Modify path to the output location
 )
